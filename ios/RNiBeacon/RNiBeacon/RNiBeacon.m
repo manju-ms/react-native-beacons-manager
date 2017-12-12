86c7e438061698c7beb209af69ab5d207d52ba90
manju <manju@mahaswami.com>
2017-12-12 14:08:39 +0530

    Beacon: Region did Exit Region Changes added.

--- ios/RNiBeacon/RNiBeacon/RNiBeacon.m
+++ ios/RNiBeacon/RNiBeacon/RNiBeacon.m
@@ -290,6 +290,9 @@ RCT_EXPORT_METHOD(shouldDropEmptyRanges:(BOOL)drop)
 
 -(void)locationManager:(CLLocationManager *)manager
         didEnterRegion:(CLBeaconRegion *)region {
+  if (! [region respondsToSelector:@selector(proximityUUID)]) {
+         return;
+  }
   NSDictionary *event = @{
                           @"identifier": region.identifier,
                           @"uuid": [region.proximityUUID UUIDString],
@@ -300,6 +303,9 @@ RCT_EXPORT_METHOD(shouldDropEmptyRanges:(BOOL)drop)
 
 -(void)locationManager:(CLLocationManager *)manager
          didExitRegion:(CLBeaconRegion *)region {
+  if (! [region respondsToSelector:@selector(proximityUUID)]) {
+      return;
+  }
   NSDictionary *event = @{
                           @"identifier": region.identifier,
                           @"uuid": [region.proximityUUID UUIDString],
